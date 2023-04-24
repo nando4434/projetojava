0d251e755c286006fd039eef4b716355be33ed00
fernando <fs745045@gmail.com>
2023-04-24 17:19:21 -0300

    orimeiro dia aula java

---
+++ scripts_js/scrip.js
@@ -0,0 +1,25 @@
+let nome ="fernando";
+let idade = 26;
+
+
+let string = "fernando";
+let number = 30;
+let booleano = true;
+let arrey = ["ovo","farinha","leite"];
+let objeto = {nome:"fernando",idade:26};
+
+
+/* exercicios*/
+let carro = "ferrari";
+let bolo = 25;
+let endereco = "rua 3";
+
+console.log("nome do carro", carro);
+console.log("preco do bolo", bolo);
+console.log("endereco", endereco);
+
+/* aula 10# */
+
+let nando = "fernando"; // e utilizada dentro do contexto que foi criada
+var nando = "fernando"; // funciona em qualquer contexto pondendo ser usada em outras variaveis
+const nando = "fernando";// essa variavel se torna contante , nao pode ser alterada
