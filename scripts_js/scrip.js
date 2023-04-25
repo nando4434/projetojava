5d4502774cc5560ca8b6adc29efcce3d3fa2bfcf
fernando <fs745045@gmail.com>
2023-04-25 18:02:08 -0300

    aula 14 ==

--- scripts_js/scrip.js
+++ scripts_js/scrip.js
@@ -25,4 +25,49 @@ var nand0 = "fernando"; // funciona em qualquer contexto pondendo ser usada em o
 const nand1 = "fernando";// essa variavel se torna contante , nao pode ser alterada
 
 var chefe = "silvio";
-console.log("nome do chefe", chefe)
+console.log("nome do chefe", chefe);
+
+// proxima aula #11
+
+
+let  nomes = "fernando";
+let sobreNome = "silva";
+
+let nomeCompleto = ` ${nome} ${sobreNome } idade 90`; //template string
+console.log(nomeCompleto);
+
+// condicional if /else
+
+let idade13 = 50;
+
+
+
+if (idade13 >= 60){
+
+    console.log("idoso");
+} 
+else{
+    console.log("flor da idade");
+}
+
+//condicional == e === #14
+
+
+let teste = 20;
+
+if (teste == "20"){
+    console.log("funcionou");
+}
+
+// ele faz uma leitura nao tao restrita , esse exemplo executou apeas 
+// dele ser um int misturado com string
+
+let teste2 = 20;
+
+if (teste2 === "20"){
+    console.log("nao funciona");
+}
+else{
+    console.log("nao funciona")
+}
+// dessa maneira ele é mais restrito mais rigorozo, mais exato
