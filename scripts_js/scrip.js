080bd68de8a654cfc44b3d1b959eb9c786c93589
fernando <fs745045@gmail.com>
2023-04-26 20:49:15 -0300

    dia completo quarta

--- scripts_js/scrip.js
+++ scripts_js/scrip.js
@@ -1,3 +1,5 @@
+
+
 let nome ="fernando";
 let idade = 26;
 
@@ -71,3 +73,46 @@ else{
     console.log("nao funciona")
 }
 // dessa maneira ele é mais restrito mais rigorozo, mais exato
+
+//aula 15
+
+var idade15 = 12;
+
+if(idade15 >= 20){
+    if(idade15 < 60){ //ele só ira executar esse comamndo se a primeira condiçao for satisfeita
+        console.log("voce e adulto");
+
+    }
+
+}
+
+if (idadae >18 && idade <60){ //as duas condicoe devem sewr satisfeita
+    console.log("ta otimo");
+}
+
+if(idade>-17 || idade<90){ // tag usada para "ou"
+    console.log("ta bom tbm");
+}
+
+
+// condicional dupla if else
+let nandera = 30;
+
+if(nandera<18){
+    console.log("!voce e criança")
+} else if(nandera>18){
+    console.log("!voce e adultoa")
+} else if(nandera>68){
+    console.log("!voce e idoso")
+}
+
+//*mais exemplos
+
+let idade17 = 30;
+
+let aduldo = idade>=18 && idade<60;
+
+if(adulto){
+    console.log("E ADULTO");
+}
+//parei na #19
